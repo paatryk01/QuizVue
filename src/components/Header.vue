0c0d3e6bb6e80c793f6df524d91706e725fda53a
paatryk01 <paatryk01@yahoo.pl>
2020-07-11 14:37:53 +0200

    add all

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled>Quiz App</b-nav-item>
+      <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
+    </b-nav>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    numCorrect: Number,
+    numTotal: Number
+  }
+}
+</script>
+
+<style>
+
+</style>
